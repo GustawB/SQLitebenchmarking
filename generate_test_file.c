a4288506732e8f71820e20285cbccdf56c394c35
GustawB <gustaw.blachowski@gmail.com>
2025-11-07 19:29:33 +0000

    add batching

--- generate_test_file.c
+++ generate_test_file.c
@@ -4,51 +4,63 @@
 #include <time.h>
 #include "sqlite3.h"
 
-#define NR_OF_ROWS 100000
-#define x1_pos 30
-#define x1_len 6
-#define x2_pos 39
-#define x2_len 4
-#define x3_pos 45
-#define x3_len 6
-#define x4_pos 53
-#define x4_len 6
-#define x5_pos 62
-#define x5_len 16
+#define NR_OF_BATCHES 1000
+#define TEXT_OFFSET 1
+#define NUMERIC_OFFSET 9
+#define INTEGER_OFFSET 14
+#define REAL_OFFSET 21
+#define BLOB_OFFSET 29
+#define BLOB_END 48
+#define INSERT_SQL_BEGINNING_LEN 29
+#define BATCH_SIZE 100
 
 const char* CREATE_TABLE_SQL = "CREATE TABLE test_data (x1 TEXT, x2 NUMERIC, x3 INTEGER, x4 REAL, x5);";
-const char* INSERT_SQL = "INSERT INTO test_data VALUES(\'______\', ____, ______, ______, \'________________\');";
+const char* INSERT_SQL_BEGINNING = "INSERT INTO test_data VALUES ";
+
+void fill_insert_values(char* buf) {
+    srand(time(NULL));
+    buf[0] = '\'';
+    for (int i = TEXT_OFFSET; i < NUMERIC_OFFSET - 2; ++i) {
+        buf[i] = (char)((rand() % 26) + 65);
+    }
+    buf[NUMERIC_OFFSET - 2] = '\'';
+    buf[NUMERIC_OFFSET - 1] = ',';
+    for (int i = NUMERIC_OFFSET; i < INTEGER_OFFSET - 1; ++i) {
+        buf[i] = (char)((rand() % 10) + 48);
+    }
+    buf[INTEGER_OFFSET - 1] = ',';
+    for (int i = INTEGER_OFFSET; i < REAL_OFFSET - 1; ++i) {
+        if (i - INTEGER_OFFSET == 3) buf[i] = '.';
+        else buf[i] = (char)((rand() % 10) + 48);
+    }
+    buf[REAL_OFFSET - 1] = ',';
+    for (int i = REAL_OFFSET; i < BLOB_OFFSET - 2; ++i) {
+        if (i - REAL_OFFSET == 3) buf[i] = '.';
+        else buf[i] = (char)((rand() % 10) + 48);
+    }
+    buf[BLOB_OFFSET - 2] = ',';
+    buf[BLOB_OFFSET - 1] = '\'';
+    for (int i = BLOB_OFFSET; i < BLOB_END - 3; ++i) {
+        buf[i] = (char)((rand() % 26) + 97);
+    }
+    buf[BLOB_END - 3] = '\'';
+}
 
 int exec_insert_sql(sqlite3 *db) {
-    char text[x1_len];
-    char blob[x5_len];
-    char integer[x2_len];
-    char numeric[x3_len];
-    srand(time(NULL));
     sqlite3_stmt *insert_stmt;
+    int total_query_size = INSERT_SQL_BEGINNING_LEN + BATCH_SIZE * (BLOB_END + 1) + 1;
 
-    char *curr_insert = malloc(strlen(INSERT_SQL) + 1);
-    strcpy(curr_insert, INSERT_SQL);
+    char *curr_insert = malloc(total_query_size);
+    memcpy(curr_insert, INSERT_SQL_BEGINNING, INSERT_SQL_BEGINNING_LEN);
 
-    for (int i = 0; i < x1_len; ++i) {
-        text[i] = (char)((rand() % 26) + 65);
+    for (int i = 0; i < BATCH_SIZE; ++i) {
+        curr_insert[INSERT_SQL_BEGINNING_LEN + i * (BLOB_END + 1)] = '(';
+        fill_insert_values(&curr_insert[INSERT_SQL_BEGINNING_LEN + i * (BLOB_END + 1) + 1]);
+        curr_insert[INSERT_SQL_BEGINNING_LEN + i * (BLOB_END + 1) + BLOB_END - 1] = ')';
+        curr_insert[INSERT_SQL_BEGINNING_LEN + i * (BLOB_END + 1) + BLOB_END] = ',';
     }
-    for (int i = 0; i < x2_len; ++i) {
-        integer[i] = (char)((rand() % 10) + 48);
-    }
-    for (int i = 0; i < x3_len; ++i) {
-        if (i == 3) numeric[i] = '.';
-        else numeric[i] = (char)((rand() % 10) + 48);
-    }
-    for (int i = 0; i < x5_len; ++i) {
-        blob[i] = (char)((rand() % 26) + 97);
-    }
-
-    memcpy(curr_insert + x1_pos, &text, x1_len);
-    memcpy(curr_insert + x2_pos, &integer, x2_len);
-    memcpy(curr_insert + x3_pos, &numeric, x3_len);
-    memcpy(curr_insert + x4_pos, &numeric, x4_len);
-    memcpy(curr_insert + x5_pos, &blob, x5_len);
+    curr_insert[total_query_size - 2] = ';';
+    curr_insert[total_query_size - 1] = '\0';
 
     int rc = sqlite3_prepare_v2(db, curr_insert, strlen(curr_insert), &insert_stmt, NULL);
     if(rc != SQLITE_OK){
@@ -61,6 +73,7 @@ int exec_insert_sql(sqlite3 *db) {
         fprintf(stderr, "Inserting into table failed\n");
         return 1;
     }
+    free(curr_insert);
     return 0;
 }
 
@@ -80,7 +93,7 @@ int main(int argc, char **argv){
         sqlite3_close(db);
         return 1;
     }
-
+    
     rc = sqlite3_prepare_v2(db, CREATE_TABLE_SQL, strlen(CREATE_TABLE_SQL), &create_table_stmt, NULL);
     if(rc != SQLITE_OK){
         fprintf(stderr, "Preparing create stmt failed\n");
@@ -93,12 +106,12 @@ int main(int argc, char **argv){
         return 1;
     }
 
-    for (int i = 0; i < NR_OF_ROWS; ++i) {
+    for (int i = 0; i < NR_OF_BATCHES; ++i) {
+        printf("%d\n", i);
         if (exec_insert_sql(db)) {
             return 1;
         }
     }
-
     sqlite3_close(db);
     return 0;
 }
